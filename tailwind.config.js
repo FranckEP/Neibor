66a14e841b88f1800ab6798ac6cbe5727acf6da4
Franck EP <franck200402@gmail.com>
2025-04-22 23:05:21 -0500

    Adding Tailwind CSS variables

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Verdana', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          light: '#8ECAE6',
+          DEFAULT: '#219EBC',
+          dark: '#023047',
+        },
+        secondary: {
+          light: '#FFB703',
+          DEFAULT: '#FB8500',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
